72175cd93ec4a46a28689304d6584bdad69d4068
Nova-022005 <sauravsingh8806@gmail.com>
2026-02-22 17:50:07 +0530

    thhis theory

--- function/02_function.js
+++ function/02_function.js
@@ -14,12 +14,12 @@ const user={
   
 }
 
-user.welcomeMessage() //*Saurav, welcome to website.
+//user.welcomeMessage() //*Saurav, welcome to website.
 user.username="Naveen"
 
-user.welcomeMessage() //* Naveen, welcome to websit.
+//user.welcomeMessage() //* Naveen, welcome to websit.
 
-console.log();//! empty in case of node 
+//console.log(this);//! return {} empty in case of node 
               //?or any other runtime like bun 
        //* But, in browser it give window obkect as global objects.
 
@@ -29,4 +29,22 @@ function chai(){
   
 }
 
-chai() 
+// chai() 
+
+//* this = undefined (strict mode) or global object (non-strict)
+
+///////////////////////////////////////////////
+
+//? this is like a contextual reference variable. But the subtlety is:
+//* In normal functions, the context is dynamic (decided at call time).
+//! In arrow functions, the context is lexical (decided at definition time).
+
+////////////////////////////////////////////////
+
+//*___________ARROW FUNCTION
+const chai02 = ()=> {
+  console.log(this);
+  
+} 
+
+chai02()//? return {}
