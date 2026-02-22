6b1dc1bfa0e2db7d650898dcb772eb590b961fe4
Nova-022005 <sauravsingh8806@gmail.com>
2026-02-22 17:24:34 +0530

    this keyword

--- function/02_function.js
+++ function/02_function.js
@@ -1,2 +1,32 @@
 //*___________Arrow Function
 
+const user={
+  username:"Saurav",
+  price:999,
+
+  welcomeMessage: function(){
+    //! this keyword is used for current context.
+    console.log(`${this.username}, welcome to websit.`);
+
+    // console.log(this);
+    
+  }
+  
+}
+
+user.welcomeMessage() //*Saurav, welcome to website.
+user.username="Naveen"
+
+user.welcomeMessage() //* Naveen, welcome to websit.
+
+console.log();//! empty in case of node 
+              //?or any other runtime like bun 
+       //* But, in browser it give window obkect as global objects.
+
+
+function chai(){
+  console.log(this.username);//* return Object [global]
+  
+}
+
+chai() 
