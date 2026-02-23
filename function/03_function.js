1c31e24795e6d9e88ecc8e52d210c7c242dc3bb8
Nova-022005 <sauravsingh8806@gmail.com>
2026-02-23 23:34:22 +0530

    scope

--- function/03_function.js
+++ function/03_function.js
@@ -1,5 +1,17 @@
-//?   _________Immediately Invoked Function Expression_________
+//?   _________________Immediately Invoked Function Expression____
 
 ///*Reason:
 //?  immediately connect database whe website is loaded
 //? to avoid global Pollution
+
+
+function chai() {
+  console.log("Database Connected.");
+}
+chai();
+
+//! IIFE
+//?* first () for fun declaration section for call\
+(function chai1(){
+  console.log("DB CONNECTED.");
+})()
