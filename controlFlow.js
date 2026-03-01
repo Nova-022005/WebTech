119d22a98640ccdabd177d6774777dd83a45518e
Nova-022005 <sauravsingh8806@gmail.com>
2026-03-01 23:00:14 +0530

    commit

---
+++ controlFlow.js
@@ -0,0 +1,15 @@
+// conditional
+
+let a=1;
+let b="1";
+if(a===b){
+  console.log("same and equal");
+}
+else{
+console.log("not same");
+}
+//not same
+
+//!  === is for matching both datadtype and value
+//? == only for value
+//? != not eual and just !x not x
