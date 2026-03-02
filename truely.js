43e94ae62f3f877f4ea5df08b8224fe903f837d2
Nova-022005 <sauravsingh8806@gmail.com>
2026-03-02 18:58:35 +0530

    Falsy and truly values

---
+++ truely.js
@@ -0,0 +1,13 @@
+let email="xyz@kiet.edu";
+
+if(email){
+  console.log(`Logged email is ${email}`);
+  
+}else{
+  console.log(`Not logged in.`);
+  
+}
+
+//! falsy values
+
+//?  false , 0, -0 ,BigInt 0n, "" ,null , undefined, Nan
