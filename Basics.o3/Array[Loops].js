2f693bb05911fe1a3f0c43088831acd0e7d2e749
Nova-022005 <sauravsingh8806@gmail.com>
2026-03-04 21:03:50 +0530

    dom

---
+++ Basics.o3/Array[Loops].js
@@ -0,0 +1,21 @@
+//? For of loop
+
+const nums=[1,2,3,44,4];
+
+for(const num of nums){
+  // console.log(num);
+}
+
+let greetings="hello World !";
+
+for(const greet of greetings){
+  // console.log(greet);
+  
+}
+
+
+// Maps in Javascript
+
+const map= new Map();
+
+map.set("saurav",1);
