2ed48e77f42522ab045516c632294177dea316cd
Nova-022005 <sauravsingh8806@gmail.com>
2026-04-05 15:37:05 +0530

    ok

--- AsynchronousCode/Projects/UnlimitedColor/color.js
+++ AsynchronousCode/Projects/UnlimitedColor/color.js
@@ -6,23 +6,33 @@ const backColor=document.body;// ? more direct approch.
 let intervalID=null;
 
 start.addEventListener('click',function(){
-  if(intervalID==null)
+  if(intervalID==null) //! only when first time and after 
   intervalID=setInterval(changeColor,700);
 })
 
 end.addEventListener('click',function(){
   if(intervalID!=null){
     clearInterval(intervalID);
-    intervalID=null
+    intervalID=null// ! flush out
   }
 })
 
 // change backgound color function
-const changeColor= function(){
-  let red=parseInt(Math.random()*256);
-  let green=parseInt(Math.random()*256);
-  let blue=parseInt(Math.random()*256);
-  let color=`rgb(${red},${green},${blue})`;
+// const changeColor= function(){
+//   let red=parseInt(Math.random()*256);
+//   let green=parseInt(Math.random()*256);
+//   let blue=parseInt(Math.random()*256);
+//   let color=`rgb(${red},${green},${blue})`;
+//   backColor.style.backgroundColor=color;
+//   console.log(color);
+// }
+
+// hex code generator
+const changeColor=()=>{
+  const hex="0123456789ABCDEF"
+  let color="#";
+  for(let i=0;i<6;i++) color+=hex[Math.floor(Math.random()*16)];
+
   backColor.style.backgroundColor=color;
   console.log(color);
   
