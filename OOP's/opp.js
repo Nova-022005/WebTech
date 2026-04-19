f7699c6073ac6838236341c24373007383844325
Nova-022005 <sauravsingh8806@gmail.com>
2026-04-19 14:09:10 +0530

    fix: correct syntax error in console.log for userOne

--- OOP's/opp.js
+++ OOP's/opp.js
@@ -30,5 +30,5 @@ function User(username,loginCount,isLoggedIn){
 
 const userOne=new User("Saurav",12,true);
 const userTwo=new User("Naveen",32,false)
-console.log(userOne.);
+console.log(userOne );
 // console.log(userTwo);
